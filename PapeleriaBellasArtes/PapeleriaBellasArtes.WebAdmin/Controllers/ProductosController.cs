66e47726c5320d37ea86b5bc3148d9e9416d4283
Kimberly Banegas <90736377+kimbanegas15@users.noreply.github.com>
2022-02-09 18:18:50 -0600

    Agregando Entity Framework Code First

---
+++ PapeleriaBellasArtes/PapeleriaBellasArtes.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,41 @@
+﻿using PapeleriaBellasArtes.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeleriaBellasArtes.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _productosBl;
+
+        public ProductosController()
+        {
+            _productosBl = new ProductosBL();
+        }
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var listadeProductos = _productosBl.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            var nuevoProducto = new Producto(); 
+
+            return View(nuevoProducto); 
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Producto producto)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
